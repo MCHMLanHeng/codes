35438a36c2636f0cc63aa00879e72d82f2b818e3
MCHMLanHeng <128813587+MCHMLanHeng@users.noreply.github.com>
2024-01-12 17:26:27 +0800

    Add files via upload

--- main.py
+++ main.py
@@ -1,17 +1,14 @@
 import tkinter as tk
 from openai import OpenAI
+import sqlite3_connector
+import sqlite3_searcher
 #初始化tkinter
 win = tk.Tk()
 win.title('text')
 win.geometry('800x800')
-def question():
-    global ans
-    cannot_send_messages = ['']
-    send_message = input()  #输入问题
-    for i in cannot_send_messages:
-        if i in send_message:
-            print('包含敏感信息,无法发送')
-            return 1
+
+def gpt():
+    global send_message,ans
     client = OpenAI(
         api_key='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'
     )
@@ -23,6 +20,20 @@ def question():
         ]
     )
     ans = completion.choices[0].message
+def question():
+    global ans,send_message
+    cannot_send_messages = ['']
+    send_message = input()  #输入问题
+    if sqlite3_searcher.search(send_message) != '':
+        print(sqlite3_searcher.search(send_message))
+    else:
+        for i in cannot_send_messages:
+            if i in send_message:
+                print('包含敏感信息,无法发送')
+                return 1
+        gpt()
+        sqlite3_connector.sql_add(ans,send_message)
+
 
 #tkinter
 First_Label = tk.Label(win,text='Text')
